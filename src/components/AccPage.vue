abfc13ade5a2ca7d5f85f03917f7a2765a2d0927
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-04-08 17:32:29 +0200

    ajout de la connexion avec ma bdd mongo, dans .env

--- src/components/AccPage.vue
+++ src/components/AccPage.vue
@@ -5,8 +5,9 @@
       </div>
       <ul class="flex justify-end space-x-4">
         <li><router-link to="/" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Home</router-link> | </li>
-        <li><router-link to="/login" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Login</router-link> | </li>
-        <li><router-link to="/chat" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Chat</router-link></li>
+        <li><router-link to="/chat" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Chat</router-link> | </li>
+        <li><router-link to="/login" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Login</router-link></li>
+
         
       </ul>
     </div>
@@ -14,16 +15,45 @@
     <div class="flex flex-col items-center h-screen mt-10">
         <h1 class="text-3xl text-center">Présentation du projet</h1>
         <div class="mt-4 text-center">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta nostrum officiis, doloribus et repellat, eos aliquam saepe magnam alias dicta labore doloremque? Debitis ad sunt repudiandae voluptatum saepe architecto sequi.</p>
+          <p>Chatify, est une messagerie instantanée, utilisant les WebSockets</p>
         </div>
+        <div class="flex">
+          <div class="w-1/2">
+            <p class="text-left text-lg font-normal w-[550px] relative top-[150px] left-[450px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iste alias aut, quos minus quaerat. Reprehenderit molestiae placeat voluptatum? Magni dolore amet quia blanditiis aut. Tempore tempora illum ipsa error.</p>
+          </div>
+          <div class="w-1/2">
+            <img src="../assets/img/img.jpg" alt="" class="float-right relative top-[130px] left-[500px] rounded-2xl w-[650px]">
+          </div>
+          <!-- Image est text2 -->
+          <div class="w-1/2">
+            <p class="text-left text-lg font-normal w-[550px] relative top-[530px] left-[90px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iste alias aut, quos minus quaerat. Reprehenderit molestiae placeat voluptatum? Magni dolore amet quia blanditiis aut. Tempore tempora illum ipsa error.</p>
+          </div>
+          <div class="w-1/2">
+            <img src="../assets/img/img2.jpg" alt="" class="float-left relative top-[550px] left-[-1000px] rounded-2xl w-[850px]">
+          </div>
+        </div>
+
     </div>
+
+    <footer class="py-2">
+      <div class="container mx-auto text-center">
+        <p class="text-sm text-white-600">
+          Copyright © Julien Milants |
+          <a href="https://github.com/NapoTwiixe306" target="_blank"><i class="ml-2 fab fa-github hover:text-sky-500"></i></a>
+          <a href="https://gitlab.com/NapoTwiixe306" target="_blank"><i class="ml-2 fab fa-gitlab hover:text-sky-500"></i></a>
+          <a href="https://instagram.com/julien.mlnts" target="_blank"><i class="ml-2 fab fa-instagram hover:text-sky-500"></i></a>
+        </p>
+      </div>
+  </footer>
 </template>
 
 <style>
     p{
-        width: 650px;
         font-size: 25px;
     }
+
+
+    
 
     .Home{
         height: 100vh;
