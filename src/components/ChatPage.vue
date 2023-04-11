bc2a20c7b14b2fd88f4b989e2da63de27a6ecc1b
NapoTwiixe306 <julien.milants@gmail.com>
2023-04-11 23:47:16 +0200

    design provisoir de chatPage

--- src/components/ChatPage.vue
+++ src/components/ChatPage.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="flex items-center justify-between px-6 py-4 navbar bg-sky-800">
-    <div class="logo">
-      <router-link class="text-2xl font-bold text-white" to="/">Chatify</router-link>
+  <div class="logo">
+    <router-link class="text-2xl font-bold text-white" to="/">Chatify</router-link>
+  </div>
+  <ul class="flex items-center justify-end space-x-4">
+    <li><router-link to="/" class="relative p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Home</router-link></li>
+    <li><button v-if="isLoggedIn" @click="logout" class="p-2 transition duration-300 p-3text-white hover:bg-cyan-600 rounded-2xl"><span class="relative left-[-15px]">| </span>Déconnexion</button></li>
+  </ul>
+</div>
+
+<div class="h-screen dark">
+  <div class="test">
+    <div class="flex items-center gap-4">
+      <h1 class="mt-4 text-lg font-bold text-black">UserName</h1>
+      <span class="inline-flex items-center justify-center w-12 h-12 mt-4 bg-gray-200 rounded-full">
+        <img src="../assets/img/img.jpg" alt="Photo de profil" class="w-10 h-10 rounded-full">
+      </span>
     </div>
-    <ul class="flex justify-end space-x-4">
-      <li><router-link to="/" class="p-3 text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Home</router-link> | </li>
-      <li><button v-if="isLoggedIn" @click="logout" class="text-white transition duration-300 hover:bg-cyan-600 rounded-2xl">Déconnexion</button></li>
-    </ul>
+    <div class="container_msg">
+      <!--ici, c'est genre une border a coter de username, la border c'est ce qui va contenir le chat chat, donc une border pas trop grande, mais grande quand même,
+         et vazy, fini le design de userName, pour afficher le user, lui permttre d'ajoute une pp, et afficher une searchBar avec la liste de dm-->
+    </div>
   </div>
-  <div class="h-screen dark">
-    <h1>hello world</h1>
-  </div>
+</div>
+
 </template>
 
 <script lang="js">
@@ -37,3 +50,17 @@ export default {
   }
 };
 </script>
+
+<style>
+  .test{
+    position: relative;
+    top: 65px;
+    margin-left: 65px;
+    border-radius: 19px;
+    bottom: 15px;
+    right: 15px;
+    width: 95%;
+    height: 90%;
+    background-color: rgba(175, 175, 175, 0.643);
+}
+</style>
